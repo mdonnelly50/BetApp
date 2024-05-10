d22a3f1acd6769b4418b6cc187eda499086414bc
Mike Donelly <mdonnelly@haverfordsd.net>
2024-05-10 07:53:47 -0400

    Initial Commit

---
+++ MikeAppFinal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MikeAppFinal
+//
+//  Created by xcode15 on 5/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MikeAppFinal/MikeAppFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MikeAppFinalApp.swift
+//  MikeAppFinal
+//
+//  Created by xcode15 on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MikeAppFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
